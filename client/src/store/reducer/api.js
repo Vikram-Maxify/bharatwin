ed0553b191782827d8fcde90bcecf4de688b3294
Vikram Thakur <trivikramkumar@maxifysolution.com>
2026-08-17 14:42:26 +0530

    by vikram

--- client/src/store/reducer/api.js
+++ client/src/store/reducer/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: `https://1xclube.com/api`,
-  // baseURL: `http://localhost:7613/api`,
+  // baseURL: `https://1xclube.com/api`,
+  baseURL: `http://localhost:7613/api`,
   // baseURL: `/api`,
 });
-// export const host = "http://localhost:7613";
-export const host = "https://1xclube.com";
+export const host = "http://localhost:7613";
+// export const host = "https://1xclube.com";
 // export const host = "/";
