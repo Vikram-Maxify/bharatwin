33fd679d19a43f625585deb967ce16a7f3b2ac7e
Vikram Thakur <trivikramkumar@maxifysolution.com>
2026-08-18 13:34:06 +0530

    Update changes

--- client/src/pages/home/Apkdownload.js
+++ client/src/pages/home/Apkdownload.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { IoCloseCircleOutline } from "react-icons/io5";
 
 import { useSelector } from "react-redux";
 
 const Apkdownload = () => {
   const [isOpen, setIsOpen] = useState(true); // Renamed 'close' to 'isOpen' for clarity
-  const {bannergetData}=useSelector((state)=>state.user)
+  const { bannergetData } = useSelector((state) => state.user);
 
   const handleClose = () => {
     setIsOpen(false); // Close the popup when 'X' is clicked
@@ -17,12 +17,16 @@ const Apkdownload = () => {
         <div className="z-40 fixed bottom-20 left-1/2 transform -translate-x-1/2">
           <div className=" px-[10px] flex blue-linear2 justify-around w-[182px] py-1 rounded-3xl shadow-lg cursor-pointer h-full items-center text-center">
             <div className="mt-1">
-              <img className="w-8 h-6" src={bannergetData?.gameall?.favicon} alt="" />
+              <img
+                className="w-8 h-6"
+                src={bannergetData?.gameall?.favicon}
+                alt=""
+              />
             </div>
-            <a href="/app.apk" download>
+            <a href="/app-release.apk" download>
               <p className="text-sm font-bold text-black">Add Desktop</p>
             </a>
-         
+
             <div
               className="cursor-pointer text-black border-l border-[var(--oranges)] text-right text-2xl font-bold mt-[2px]"
               onClick={handleClose}
